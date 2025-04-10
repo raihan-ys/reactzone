ca9dd92c3dcb15e1c62b56a46e15a3054e04ff36
_RAINHARDER <raihanys03@gmail.com>
2025-04-10 20:45:20 +0700

    Update

--- components/ListGroup.tsx
+++ components/ListGroup.tsx
@@ -1,17 +1,20 @@
-const items = [
+let items = [
   'Bukittinggi',
   'Padang',
   'Payakumbuh',
   'Padang Panjang'
 ];
 
+items = [];
+
 function ListGroup() {
   return (
     <>
-      <h1 className="text-primary">List</h1>
+      <h1 className="text-danger">List</h1>
+      { items.length === 0 ? <p>No items found!</p> : null }
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item} className="list-group-item text-danger">
+          <li key={item} className="list-group-item" style={{ color: 'orangered' }}>
             {item}
           </li>
         ))}
