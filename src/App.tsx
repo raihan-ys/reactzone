f538a11becaca3a390353177adce40f4c82bc3c7
_RAINHARDER <rainharder03@gmail.com>
2025-07-18 12:09:21 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import QuizForm from '../components/QuizForm'
 function App() {
   // This is the child element.
   return <>
-    <QuizForm headingStyle='text-danger font-weight-bold' paragraphStyle='text-secondary' textareaStyle='rounded border-danger' buttonStyle='btn btn-sm btn-danger' errorStyle='text-danger bold' />
+    <QuizForm headingStyle='text-danger fw-bold' paragraphStyle='text-secondary' textareaStyle='rounded border-danger' buttonStyle='btn btn-sm btn-danger' errorStyle='text-danger fw-bold' />
   </>
 }
 
