3f13141af1ec57a5db9660fd48f264baf57f7183
Raihan <raihanys03@gmail.com>
2026-02-25 10:30:17 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import CityQuiz from '../components/CityQuiz';
 function App() {
   // TODO: Create a main menu that list all the games
   return <>
-    Welcome to Reactzone!
-    Choose a game:
+    <h4 className="text-primary" id="welcome-text">Welcome to Reactzone!</h4>
+    <h5>Choose a game:</h5>
     <ul style={{ listStyleType: 'square' }}>
       <li>City Quiz</li>
       <li>Tic Tac Toe (2 Players)</li>
